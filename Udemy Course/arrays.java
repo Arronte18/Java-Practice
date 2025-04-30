ab2444b6a5d94de9ef9ff66aba0145e02400ac9b
Arronte18 <anthonyarronte@gmail.com>
2025-04-30 18:02:26 -0400

    small update on section9

--- Udemy Course/arrays.java
+++ Udemy Course/arrays.java
@@ -34,5 +34,44 @@ public class arrays {
 
         //---------------------------------------------------------------
         System.out.println("---------------------------------------------");
+
+        int [] testy = getRandomArray(12);
+        //modified merge sort that runs at n*logn
+        Arrays.sort(testy);
+        System.out.println(Arrays.toString(testy));
+
+        //initialization sets default values of 0
+        int[] secondArray = new int[10];
+        System.out.println(Arrays.toString(secondArray));
+        //sets all elements to 5
+        Arrays.fill(secondArray, 5);
+        System.out.println(Arrays.toString(secondArray));
+
+
+        int[] thirdArray = getRandomArray(10);
+        System.out.println(Arrays.toString(thirdArray));
+
+        int[] fourthArray = Arrays.copyOf(thirdArray, thirdArray.length);
+        System.out.println(Arrays.toString(fourthArray));
+
+        Arrays.sort(fourthArray);
+        System.out.println(Arrays.toString(thirdArray));
+        System.out.println(Arrays.toString(fourthArray));
+
+        int[] smallerArray = Arrays.copyOf(thirdArray, 5);
+        System.out.println(Arrays.toString(smallerArray));
+
+        int[] largerArrray = Arrays.copyOf(thirdArray, 20);
+        System.out.println(Arrays.toString(largerArrray));
+        
+    }
+    private static int[] getRandomArray(int len){
+        Random random = new Random();
+        int[] newInt = new int[len];
+
+        for(int i = 0; i < len; i++){
+            newInt[i] = random.nextInt(100);
+        }
+        return newInt;
     }
 }
